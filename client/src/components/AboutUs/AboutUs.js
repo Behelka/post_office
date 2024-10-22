7a19f365e228fc617b8952b32e6ae17d60785d3d
AyanAnees <ayananees007@gmail.com>
2024-10-22 15:22:26 -0500

    login page

--- client/src/components/AboutUs/AboutUs.js
+++ client/src/components/AboutUs/AboutUs.js
@@ -1,10 +1,43 @@
 import React from "react";
-import "./AboutUs.css"
+import "./AboutUs.css";
 
-function AboutUs() {
-    return(
-        <div/>
-    );
-}
+const AboutUs = () => {
+  return (
+    <div className="about-container">
+      <div className="about-content">
+        <h1>About Us</h1>
+        <p>
+          Welcome to the Post Office Service! We are dedicated to providing
+          fast, reliable, and secure mailing and shipping services to meet your
+          personal and business needs. Whether you're sending letters, packages,
+          or important documents, our platform ensures timely deliveries and
+          easy tracking of your items.
+        </p>
+        <p>
+          Our commitment to quality service stems from decades of experience in
+          logistics and communication. We strive to bring cutting-edge
+          technology and traditional postal values together, allowing customers
+          to enjoy a seamless experience in all their postal interactions.
+        </p>
+        <p>
+          At the Post Office Service, we value trust, efficiency, and customer
+          satisfaction. With our broad network of local and international
+          shipping partners, you can send parcels to any part of the world with
+          confidence.
+        </p>
+        <h2>Why Choose Us?</h2>
+        <ul>
+          <li>Secure and reliable delivery service</li>
+          <li>
+            User friendly tracking and management tools for all deliveries
+          </li>
+          <li>Dedicated Customer Support and fast resolution of issues</li>
+          <li>Timely and efficient domestic shipping</li>
+        </ul>
+        <p>Thank you for choosing Cougar</p>
+      </div>
+    </div>
+  );
+};
 
 export default AboutUs;
---
+++ client/src/components/ContactUS/contact.js
@@ -0,0 +1,10 @@
+// import React from 'react'
+// import './contact.css'
+
+// const contact=()=>{
+//     const [email,setEmail]=useState('')
+//     const [question,setQuestion]=useState('')
+//     const [error,setError]=useState('')
+// }
+
+
