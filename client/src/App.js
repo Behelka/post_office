bfd7143ac92709146d380e3c4b289252ec778719
JasonPedder <158420747+JasonPedder@users.noreply.github.com>
2024-10-24 22:39:25 -0500

    Added semi functional package portal

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Shop from "./components/Shop/Shop";
 import SignUp from "./components/SignUp/SignUp";
 import TrackingHistory from "./components/TrackingHistory/TrackingHistory";
 import EmployeeShop from "./components/Shop/EmployeeShop";
+import Stops from "./components/PackagePortal/Stops";
 
 const App = () => {
   let links = [
@@ -92,6 +93,7 @@ const App = () => {
         <Route path="/AddDepartment" element={<AddDepartment />} />
         <Route path="/AddLocation" element={<AddLocation />} />
         <Route path="/EmployeeShop" element={<EmployeeShop />} />
+        <Route path="/stops/:packageId" element={<Stops />} />
       </Routes>
     </Router>
   );
--- server/Routes/appRoute.js
+++ server/Routes/appRoute.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const handleLocationRoutes = require('./AddLocationRoute'); // Adjust the path as necessary
 const handleCustomerRoutes = require('./CustomerProfileRoute'); // Add other routes as necessary
+const handlePackagePortalRoutes = require('./PackagePortalRoute');
+const handleStopRoutes = require('./StopRoute');
 
 const appRoute = (req, res) => {
     const parsedUrl = url.parse(req.url, true);
@@ -11,10 +13,14 @@ const appRoute = (req, res) => {
         handleLocationRoutes(req, res);
     } else if (parsedUrl.pathname.startsWith('/api/customer')) {
         handleCustomerRoutes(req, res);
+    } else if (parsedUrl.pathname.startsWith('/api/PackagePortal')){
+        handlePackagePortalRoutes(req, res);
+    } else if (parsedUrl.pathname.startsWith('/api/Stops')) {
+        handleStopRoutes(req, res);
     } else {
         res.writeHead(404, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ message: 'Not Found' }));
-    }
+    } 
 };
 
 module.exports = appRoute;
